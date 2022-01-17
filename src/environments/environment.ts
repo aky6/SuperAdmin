c30d68e24662f77d12000e69437a6af53540229a
Akash Dubey <akash.dubey@philom.in>
2022-01-17 21:45:31 +0530

    all

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000',
+  apiUrl: 'https://homefood-backend.herokuapp.com',
   // 'https://homeade.herokuapp.com'
 };
 
