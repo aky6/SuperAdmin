55016fac645af86ad83f2f8b10301fc7f095cf1e
Akash Dubey <akash.dubey@philom.in>
2022-02-03 16:40:00 +0530

    all

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // apiUrl: 'https://homefood-backend.herokuapp.com',
-  apiUrl: 'https://homeade.herokuapp.com',
+  apiUrl: 'https://homefood-backend.herokuapp.com',
   // 'https://homeade.herokuapp.com'
 };
 
